acb64936002455a7f1f11566ca44d3cd2b3090bc
Vlatko Stojkoski <stojvlatko@gmail.com>
2023-02-13 09:33:03 +0100

    fix project link

--- src/components/PortfolioSection.tsx
+++ src/components/PortfolioSection.tsx
@@ -23,63 +23,66 @@ const PortfolioSection: React.FC<{projects: RouterOutputs['projects']['getAll'] 
 						projects &&
 						projects.data.map(({attributes}, projIdx) => (
 							<Link key={projIdx} href={attributes.links?.[0]?.[1] || '#'} target='_blank' passHref legacyBehavior>
-								<div className='bg-white p-4'>
-									<div className='w-full h-32 relative'>
-										<Image
-											className='object-cover'
-											src={env.NEXT_PUBLIC_API_URI + attributes.thumbnail.data.attributes.formats.small.url}
-											alt='Project thumbnail'
-											fill />
+								<div className='flex flex-col justify-between bg-white p-4'>
+									<div className="">
+										<div className='w-full h-32 relative'>
+											<Image
+												className='object-cover'
+												src={env.NEXT_PUBLIC_API_URI + attributes.thumbnail.data.attributes.formats.small.url}
+												alt='Project thumbnail'
+												fill />
+										</div>
+										<div className='px-4 pt-2'>
+											<div className='flex gap-2 my-3'>
+												{attributes.tags.data.map(({attributes: tag}, tagIdx) => {
+													const textColor = color(tag.color).isDark() ? 'white' : 'black';
+
+													return (
+														<span
+															key={tagIdx}
+															className='rounded-full px-3 py-1 text-sm font-semibold'
+															style={{background: tag.color, color: textColor}}>
+															{tag.name}
+														</span>
+													);
+												})}
+											</div>
+											<h2 className='text-xl font-semibold text-gray-800'>{attributes.title}</h2>
+											<p className='text-gray-700'>
+												{attributes.short}
+											</p>
+										</div>
 									</div>
-									<div className='px-4 pt-2'>
-										<div className='flex gap-2 my-3'>
-											{attributes.tags.data.map(({attributes: tag}, tagIdx) => {
-												const textColor = color(tag.color).isDark() ? 'white' : 'black';
 
-												return (
-													<span
-														key={tagIdx}
-														className='rounded-full px-3 py-1 text-sm font-semibold'
-														style={{background: tag.color, color: textColor}}>
-														{tag.name}
-													</span>
-												);
-											})}
-										</div>
-										<h2 className='text-xl font-semibold text-gray-800'>{attributes.title}</h2>
-										<p className='text-gray-700'>
-											{attributes.short}
-										</p>
-										<div className='flex gap-2 my-3'>
-											{
-												attributes.links.map(([type, href], linkIdx) => {
-													if(!href)
-														return <></>;
+									<div className='flex gap-2 my-3 px-4'>
+										{
+											attributes.links.map(([type, href], linkIdx) => {
+												if(!href)
+													return <></>;
 
-													return <Link
-														key={linkIdx}
-														href={href}
-														className={`${linkIdx === 0 ? 'bg-violet-700 text-white' : 'bg-gray-200 text-gray-700'} px-3 py-1 text-sm font-semibold`}
-														target='_blank'
-														data-hover>
-														{
-															(() => {
-																switch(type) {
-																case 'github':
-																	return <LogoGithub />;
-																case 'website':
-																	return <ContentView />;
-																case 'youtube':
-																	return <LogoYoutube />;
-																default:
-																	return <LinkIcon />;
-																}
-															})()
-														}
-													</Link>;
-												})
-											}
-										</div>
+												return <Link
+													key={linkIdx}
+													href={href}
+													className={`${linkIdx === 0 ? 'bg-violet-700 text-white' : 'bg-gray-200 text-gray-700'} px-3 py-1 text-sm font-semibold`}
+													target='_blank'
+													data-hover>
+													{
+														(() => {
+															switch(type) {
+															case 'github':
+																return <LogoGithub />;
+															case 'website':
+																return <ContentView />;
+															case 'youtube':
+																return <LogoYoutube />;
+															default:
+																return <LinkIcon />;
+															}
+														})()
+													}
+												</Link>;
+											})
+										}
 									</div>
 								</div>
 							</Link>
