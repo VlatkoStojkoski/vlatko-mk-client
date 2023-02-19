9f249b2cd76e6e1f7ea73455959eebe3d3a34abd
Vlatko Stojkoski <stojvlatko@gmail.com>
2023-02-19 18:58:53 +0100

    og tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				<meta name="description" content="Vlatko Stojkoski" />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="/assets/og-image.jpg" />
+				<meta name="twitter:card" content="summary_large_image" />
 			</Head>
 			<body className='bg-gradient-to-b from-cobalt-blue-900 to-cobalt-blue-700 text-white'>
 				<Main />
